4e7ea63b014f2a2505b6397d71d38dd51546e118
testadmin <>
2019-08-08 12:40:31 +0200

    {/src/main/java/com/myspace/cards/Person.java}

--- src/main/java/com/myspace/cards/Person.java
+++ src/main/java/com/myspace/cards/Person.java
@@ -6,12 +6,33 @@ package com.myspace.cards;
 
 public class Person implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Person() {
-    }
+	private java.lang.String name;
+	private java.lang.Double result;
 
+	public Person() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Double getResult() {
+		return this.result;
+	}
+
+	public void setResult(java.lang.Double result) {
+		this.result = result;
+	}
+
+	public Person(java.lang.String name, java.lang.Double result) {
+		this.name = name;
+		this.result = result;
+	}
 
 }
